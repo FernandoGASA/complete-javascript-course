7b1e5e2c1cda2533663f566bf7e48923603bf945
Fernando Adrián García <xpadrian2010@hotmail.com>
2026-05-07 16:58:42 -0600

    Add editorconfig, VSCode settings, and starter code
    Add .editorconfig and .vscode/settings.json to enforce consistent formatting and editor behavior. Create starter JavaScript files (script.js and assignments.js) with example variables and console logs, and update index.html to include the scripts and adjust markup/formatting for the JavaScript Fundamentals Part 1 starter project.

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+let country = 'México';
+let continent = 'América';
+//in millions of people
+let population = 132;
+
+console.log(country, continent, population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+let js = 'amazing';
+console.log(410 + 20 - 20);
+
+let firstName = "Jonas";
+const PI = 3.1415;
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
