91385adb3d60ded9c49bb4f3f7194d516b2d3911
Fernando Adrián García <xpadrian2010@hotmail.com>
2026-05-20 09:21:30 -0600

    operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -18,3 +18,20 @@ console.log(javaScriptIsFun);
 const birthYear = 1999;
 let age = 26;
 age = 27;
+
+const now = 2026;
+const ageJonas = now - 1999;
+const ageSarah = now - 2017;
+console.log(ageJonas > ageSarah);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
